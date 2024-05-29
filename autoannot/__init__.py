c9c8d515729c354f456ffbaae9c3612bd7e0c892
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-05-29 12:20:32 +0200

    added

---
+++ autoannot/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from pathlib import Path
+import sys
+
+import yaml
+
+# Root directory
+ROOT_DIR = Path(__file__).parent.parent
+
+# Configuration
+with open(ROOT_DIR / "config.yaml") as stream:
+    config = yaml.safe_load(stream)
+
+if not config["logging"]["sppas"]:
+    logging.getLogger().disabled = True  # SPPAS logs to root by default
