d49460b283bdfdcc05b92e953958b3b0df043683
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-05-29 19:43:32 +0200

    transcription

---
+++ autoannot/transcription/transcribe.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from typing import Dict
+
+from .transcribe_whisper import transcribe_whisper
+
+
+def transcribe(in_file: str | Path, out_file: str | Path, params: Dict):
+
+    backend = params["transcription"]["backend"]
+    if backend == "whisper":
+        transcribe_whisper(in_file, out_file, **params["transcribe"]["whisper"])
+    else:
+        NotImplementedError(f"Backend '{backend}' is not implemented")
