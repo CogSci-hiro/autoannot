5bbcba62aa9190f53b9a197e8ad2f548a8bf1cd3
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-05-29 12:26:23 +0200

    added

---
+++ autoannot/diarization/__init__.py
@@ -0,0 +1,10 @@
+# Default parameters for SPPAS in seconds
+MIN_SIL = 0.05
+MIN_IPU = 0.1
+SHIFT_START = 0.01
+SHIFT_END = 0.01
+MIN_MEAN_DURATION = 60
+MIN_N_IPUS = 10
+
+# Pyannote details
+PYANNOT_MODEL = "pyannote/speaker-diarization@2.1"
