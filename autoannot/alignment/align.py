b5d93ecb0a2740cc0a6c02172d36f3aace971c84
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-05-29 22:29:18 +0200

    tidy

--- autoannot/alignment/align.py
+++ autoannot/alignment/align.py
@@ -6,7 +6,7 @@ from .julius import palign
 
 def align(in_file: str | Path, out_file: str | Path, trf_file: str | Path, params: Dict) -> None:
 
-    if params["align"]["backend"] == "julius":
+    if params["alignment"]["backend"] == "julius":
         palign(in_file, out_file, trf_file)
     else:
         raise NotImplementedError(f"Backend '{params['align']['backend']}' is not supported")
