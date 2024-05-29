2a86a1853e94a60f2a42c5a0daf73d42fd8155a5
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-05-29 19:32:05 +0200

    added align_transcription.py.py

---
+++ autoannot/transcription/align_transcription.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def align_transcription(diarization_file: str | Path, transcription_file: str | Path, out_file: str | Path,
+                        mode: str = "full", tier_name: str = "transcription"):
+
+    dia_df = pd.read_csv(diarization_file)
+    trs_df = pd.read_csv(transcription_file)
+
+    results = {"tier": [], "start": [], "end": [], "annotation": []}
+    for _, row in dia_df.iterrows():
+
+        # Select matching transcriptions and discard rest
+        if mode == "full":
+
+            # Only keep transcriptions fully inside IPUs
+            selection = trs_df[(row["start"] <= trs_df["start"]) & (trs_df["end"] <= trs_df["end"])]
+
+        elif mode == "partial":
+
+            # Keep transcriptions partially overlapping
+            selection = trs_df[(row["start"] <= trs_df["start"]) | (trs_df["end"] <= trs_df["end"])]
+
+        else:
+            raise NotImplementedError(f"The 'mode' '{mode}' is not implemented")
+
+        # Matched text
+        text = selection["annotation"].to_list()
+        text = " ".join(text)
+
+        # Add the annotations
+        results["tier"].append(tier_name)
+        results["start"].append(row["start"])
+        results["end"].append(row["end"])
+        results["annotation"].append(text)
+
+    results = pd.DataFrame(results)
+    results.to_csv(out_file, index=False)
