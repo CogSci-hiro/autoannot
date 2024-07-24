4ff500556d7dc65c3b2866feeccdd61c360fe5d2
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-07-24 20:24:00 +0200

    added

--- autoannot/alignment/align.py
+++ autoannot/alignment/align.py
@@ -5,6 +5,23 @@ from .julius import palign
 
 
 def align(in_file: str | Path, out_file: str | Path, trf_file: str | Path, params: Dict) -> None:
+    """
+    Force-alignment to create phoneme level annotations
+
+    Parameters
+    ----------
+    in_file : str | Path
+        path to input file
+    out_file : str | Path
+        path to output file
+    trf_file : str | Path
+        path to transcription file
+    params : dict
+        parameter dictionary
+    Returns
+    -------
+    None
+    """
 
     if params["alignment"]["backend"] == "julius":
         palign(in_file, out_file, trf_file)
