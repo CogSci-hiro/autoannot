111bd30394f35b00ce8c434c0127207e567d8c8f
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-07-25 10:58:31 +0200

    added

--- autoannot/alignment/align.py
+++ autoannot/alignment/align.py
@@ -2,28 +2,27 @@ from pathlib import Path
 from typing import Dict
 
 from .julius import palign
+from autoannot.docs import fill_doc
 
 
-def align(in_file: str | Path, out_file: str | Path, trf_file: str | Path, params: Dict) -> None:
+@fill_doc
+def align(in_file: str | Path, out_file: str | Path, trs_file: str | Path, params: Dict) -> None:
     """
     Force-alignment to create phoneme level annotations
 
     Parameters
     ----------
-    in_file : str | Path
-        path to input file
-    out_file : str | Path
-        path to output file
-    trf_file : str | Path
-        path to transcription file
-    params : dict
-        parameter dictionary
+    %(in_file)s
+    %(out_file)s
+    %(trs_file)s
+    %(params)s
+
     Returns
     -------
     None
     """
 
     if params["alignment"]["backend"] == "julius":
-        palign(in_file, out_file, trf_file)
+        palign(in_file, out_file, trs_file)
     else:
         raise NotImplementedError(f"Backend '{params['align']['backend']}' is not supported")
