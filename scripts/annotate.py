a8fb6c55ce33debd114351cac1a8e9e95096993e
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-07-25 10:59:06 +0200

    added

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+import traceback
 from typing import Dict
 
+import pandas as pd
 from joblib import Parallel, delayed
 
 from autoannot import diarize, transcribe, align, get_wav_paths, get_path_list
@@ -21,6 +23,9 @@ def annotate(params: Dict):
     # Get WAV paths
     wav_list = get_wav_paths(src_dir)
 
+    # Save all errors
+    errors = {"file": [], "error": []}
+
     # Diarize
     if _make_diarization(dst_dir, wav_list, use_existing):
         dia_dir = _make_dirs(dst_dir, "diarizations")
@@ -29,7 +34,15 @@ def annotate(params: Dict):
         dia_list = get_path_list(dia_dir, wav_list, extension="csv", suffix="diarization")
         log_list = get_path_list(log_dir, wav_list, extension="log", suffix="error")
 
-        Parallel(n_jobs=n_jobs)(delayed(diarize)(w, o, l, params) for w, o, l in zip(wav_list, dia_list, log_list))
+        # Parallel(n_jobs=n_jobs)(delayed(diarize)(w, o, l, params) for w, o, l in zip(wav_list, dia_list, log_list))
+        for w, o, l in zip(wav_list, dia_list, log_list):
+
+            try:
+                diarize(w, o, l, params)
+
+            except Exception:  # noqa
+                errors["file"].append(os.path.basename(w))
+                errors["error"].append(traceback.format_exc())
 
     # Transcribe
     if _make_transcription(dst_dir, wav_list, use_existing, target):
@@ -39,17 +52,36 @@ def annotate(params: Dict):
         dia_dir = _make_dirs(dst_dir, "diarizations")
         dia_list = get_path_list(dia_dir, wav_list, extension="csv", suffix="diarization")
 
-        Parallel(n_jobs=n_jobs)(delayed(transcribe)(w, t, d, params) for w, t, d in zip(wav_list, trs_list, dia_list))
+        # Parallel(n_jobs=n_jobs)(delayed(transcribe)(w, t, d, params) for w, t, d in zip(wav_list, trs_list, dia_list))
+        for w, t, d in zip(wav_list, trs_list, dia_list):
+
+            try:
+                transcribe(w, t, d, params)
+
+            except Exception:  # noqa
+                errors["file"].append(os.path.basename(w))
+                errors["error"].append(traceback.format_exc())
 
     # Align
     if _make_alignment(dst_dir, wav_list, use_existing, target):
 
-        align_dir = _make_dirs(dst_dir, "aligned_transcriptions")
+        align_dir = _make_dirs(dst_dir, "transcriptions")
         align_list = get_path_list(align_dir, wav_list, extension="csv", suffix="aligned")
         palign_dir = _make_dirs(dst_dir, "palign_transcriptions")
         palign_list = get_path_list(palign_dir, wav_list, extension="TextGrid", suffix="palign")
 
-        Parallel(n_jobs=n_jobs)(delayed(align)(w, p, a, params) for w, p, a in zip(wav_list, palign_list, align_list))
+        # Parallel(n_jobs=n_jobs)(delayed(align)(w, p, a, params) for w, p, a in zip(wav_list, palign_list, align_list))
+        for w, p, a in zip(wav_list, palign_list, align_list):
+
+            try:
+                align(w, p, a, params)
+
+            except Exception:  # noqa
+                errors["file"].append(os.path.basename(w))
+                errors["error"].append(traceback.format_exc())
+
+    error_df = pd.DataFrame(errors)
+    error_df.to_csv(dst_dir / "errors.csv", index=False)
 
 
 def _make_dirs(dst_dir: Path, name: str):
