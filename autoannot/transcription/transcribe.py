88e17fb5e8e9718e4f04b65e74719bde2f26d955
hiro <cog.sci.hiro.yamasaki@gmail.com>
2024-07-25 11:01:56 +0200

    added

--- autoannot/transcription/transcribe.py
+++ autoannot/transcription/transcribe.py
@@ -2,11 +2,26 @@ from pathlib import Path
 from typing import Dict
 
 from .transcribe_whisper import transcribe_whisper
-from .transcribe_w2v2 import transcribe_w2v2
+from .transcribe_w2v2 import transcribe_wav2vec2
 from .clean_transcription import clean_transcription
+from autoannot.docs import fill_doc
 
 
-def transcribe(in_file: str | Path, out_file: str | Path, dia_file: str | Path, params: Dict):
+@fill_doc
+def transcribe(in_file: str | Path, out_file: str | Path, dia_file: str | Path, params: Dict) -> None:
+    """
+
+    Parameters
+    ----------
+    %(in_file)s
+    %(out_file)s
+    %(dia_file)s
+    %(params)s
+
+    Returns
+    -------
+    None
+    """
 
     backend = params["transcription"]["backend"]
 
@@ -14,7 +29,7 @@ def transcribe(in_file: str | Path, out_file: str | Path, dia_file: str | Path, 
         df = transcribe_whisper(in_file, dia_file, **params["transcription"]["whisper"])
 
     elif backend == "wav2vec2":
-        df = transcribe_w2v2(in_file, dia_file, **params["transcription"]["wav2vec2"])
+        df = transcribe_wav2vec2(in_file, dia_file, **params["transcription"]["wav2vec2"])
     else:
         raise NotImplementedError(f"Backend '{backend}' is not implemented")
 
